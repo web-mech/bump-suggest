ab4ac225bf156ae13981c6e088d9f2099cce43b3
webmech <webmech@gmail.com>
2017-04-15 00:07:14 -0400

    test(*): test updates
    add docs test. remove comparing github commits from tests

--- test/usage.spec.js
+++ test/usage.spec.js
@@ -1,33 +1,23 @@
 const expect = require('chai').expect;
 const bumpSuggest = require('..');
-const ocotnode = require('octonode');
-const repository = 'web-mech/bump-suggest-test';
-const client = ocotnode.client();
-const repo = client.repo(repository);
 const base = '0.1.0';
 
 describe('bump suggest', () => {
 
-  it('detects fix commits and outputs a patch release suggestion.', function (done) {
+  it('detects docs and treats as a patch', function() {
+    expect(bumpSuggest(['docs(readme): better link'], base)).to.equal('0.1.1');
+  });
+
+  it('detects fix commits and outputs a patch release suggestion.', function () {
     expect(bumpSuggest(['fix(everything): Some test commit message'], base)).to.equal('0.1.1');
-    repo.compare('master', 'patch', function(err, results) {
-      expect(bumpSuggest(results.commits.map((commit) => commit.commit.message), base)).to.equal('0.1.1');
-      done();
-    });
   });
 
-  it('detects feature commits and outputs a minor release suggestion.', function (done) {
-    repo.compare('master', 'minor', function(err, results) {
-      expect(bumpSuggest(results.commits.map((commit) => commit.commit.message), base)).to.equal('0.2.0');
-      done();
-    });
+  it('detects feature commits and outputs a minor release suggestion.', function () {
+    expect(bumpSuggest(['feat(*): Some shiny new feature'], base)).to.equal('0.2.0');
   });
 
-  it('detects breaking changes and outputs a major release suggestion.', function (done) {
-    repo.compare('master', 'major', function(err, results) {
-      expect(bumpSuggest(results.commits.map((commit) => commit.commit.message), base)).to.equal('1.0.0');
-      done();
-    });
+  it('detects breaking changes and outputs a major release suggestion.', function () {
+    expect(bumpSuggest(['feat(*): Some shiny new feature.\nsubject\ndescription\nBREAKING CHANGE:\nthis changes everything.'], base)).to.equal('1.0.0');
   });
 });
 
