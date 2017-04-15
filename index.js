2547b411509e317d337717cda49c31e364075455
webmech <webmech@gmail.com>
2017-04-15 00:09:49 -0400

    feat(*): update module parameters
    allow parserOpts to be passed into module

--- index.js
+++ index.js
@@ -5,25 +5,28 @@ const parser = require('./lib/parser');
 const priority = {
     'break': 1,
     'feat': 2,
-    'fix': 3,
-    '': 4
+    'docs': 3,
+    'fix': 4,
+    '': 5
 };
 
 const changeReleaseMap = {
     'break': 'major',
     'feat': 'minor',
     'fix': 'patch',
+    'docs': 'patch',
     '': ''
 };
 
-module.exports = function(commits, base) {
+module.exports = function(commits, base, config) {
     let changeType = commits
-        .map((commit) => parser(commit))
+        .map((commit) => parser(commit, config))
         .filter((commit) => !!commit)
         .reduce((type, commit) => {
             if (commit.notes.length) {
                 return 'break';
             }
+
             if (priority[commit.type] <= priority[type]) {
                 return commit.type;
             }
